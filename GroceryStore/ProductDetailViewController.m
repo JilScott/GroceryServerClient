12efbc62f4265ebd973a9c8de506e3fb6793a852
Jilian Scott <jilian.scott@vokal.io>
2015-05-15 09:35:24 -0500

    implemented POST to restock product

--- GroceryStore/ProductDetailViewController.m
+++ GroceryStore/ProductDetailViewController.m
@@ -14,6 +14,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *productQuantityLabel;
 
 @property (weak, nonatomic) IBOutlet UILabel *quantityAdjustmentLabel;
+@property (weak, nonatomic) IBOutlet UIStepper *stepperOutlet;
 
 @end
 @implementation ProductDetailViewController
@@ -34,10 +35,7 @@
     NSString *productURL = [NSString stringWithFormat:@"http://127.0.0.1:5000/api/inventory/%@", self.productName];
     
     NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
-    sessionConfig.HTTPAdditionalHeaders = @{
-                                            @"product"       : self.productName,
-                                            @"Content-Type"  : @"application/json"
-                                            };
+
     NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig];
     
     NSMutableURLRequest *URLRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:productURL]];
@@ -67,7 +65,74 @@
     //resume
     [deleteProduct resume];
 }
-- (IBAction)updateQuantity:(id)sender {
+
+- (IBAction)restockProduct:(id)sender { //add
+    //TODO? Can't stock product not currently/previously in inventory (201)
+    
+    //create session
+    NSString *productURL = [NSString stringWithFormat:@"http://127.0.0.1:5000/api/inventory/%@", self.productName];
+    
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig];
+    
+    NSMutableURLRequest *URLRequest = [[NSMutableURLRequest alloc] initWithURL:[NSURL URLWithString:productURL]];
+    URLRequest.HTTPMethod = @"POST";
+    //specify amount to add
+    NSDictionary *quantityDict = @{@"quantity" : @(self.stepperOutlet.value)};
+
+    NSData *quantityToPass = [NSJSONSerialization dataWithJSONObject:quantityDict options:NSJSONWritingPrettyPrinted error:nil];
+    [URLRequest setValue:@"application/json" forHTTPHeaderField:@"Content-type"];
+
+
+    //create task
+    NSURLSessionUploadTask *restock = [session uploadTaskWithRequest:URLRequest fromData:quantityToPass completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+        
+        NSHTTPURLResponse *httpResp =
+        (NSHTTPURLResponse *) response;
+        if (httpResp.statusCode == 200) {
+            NSError *jsonError;
+            NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+            
+            if (!jsonError) {
+                NSLog(@"%@", jsonDict);
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    
+                    //update UI
+                    self.productQuantityLabel.text = [NSString stringWithFormat:@"%@", @(self.stepperOutlet.value)];
+                    
+                });
+            }
+        }
+    }];
+//    
+//    NSURLSessionDataTask *restockProduct = [session dataTaskWithRequest:URLRequest completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+//        
+//        NSHTTPURLResponse *httpResp =
+//        (NSHTTPURLResponse *) response;
+//        if (httpResp.statusCode == 200) {
+//            NSError *jsonError;
+//            NSDictionary *jsonDict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&jsonError];
+//            
+//            if (!jsonError) {
+//                NSLog(@"%@", jsonDict);
+//                dispatch_async(dispatch_get_main_queue(), ^{
+//                    
+//                    //update UI
+//                    self.productQuantityLabel.text = [NSString stringWithFormat:@"%@", @(self.stepperOutlet.value)];
+//                    
+//                });
+//            }
+//        }
+//    }];
+    
+    //resume
+    [restock resume];
+}
+
+- (IBAction)purchaseProduct:(id)sender { //subtract
+    
+    
 }
 
 @end
